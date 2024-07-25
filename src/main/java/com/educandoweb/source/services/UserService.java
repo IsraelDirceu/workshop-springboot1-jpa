1c6c47865c96aad732ad1f382b526562b02829b5
IsraelDirceu <dl.israel@gmail.com>
2024-07-25 15:12:40 -0300

    Adicionando componentes ID

---
+++ src/main/java/com/educandoweb/source/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.source.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.source.entities.User;
+import com.educandoweb.source.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
